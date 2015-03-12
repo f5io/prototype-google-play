59eb3ff2ebb66ab3ddf0bdcef7fcefa2522c8091
Joe Harlow <joe@f5.io>
2015-03-12 13:45:12 +0000

    ES & IT implementations

--- app/assets/dist/prod.js
+++ app/assets/dist/prod.js
@@ -1401,6 +1401,20 @@ var content = {
 			'3': 'Егор_Крид_Самая_самая?id=Bwfkc2eoxn2ttscoqerrpvaxaka',
 			'4': 'Наутилус_Помпилиус_Эта_музыка_будет_вечной_Nautilu?id=Bxf5rl5rbke4idfmcywpk7b7smy',
 			'5': 'Taylor_Swift_1989?id=Bm6l5gvjd6hponuuvzdzk2jwqsm'
+		},
+		es: {
+			'1': 'Sam_Smith_In_The_Lonely_Hour?id=Byxhdoru57rixsex7yk3nav7fu4&hl=es',
+			'2': 'Ed_Sheeran_x?id=Bsyd3qgzwutmzucaiknalamq7my&hl=es',
+			'3': 'Miguel_Bose_Amo?id=Boxtqhy5b3lddtrhaquffrpplny&hl=es',
+			'4': 'Avicii_The_Nights_Avicii_By_Avicii?id=Blhcdbueiandfbwpmcd22kv4aqq&hl=es',
+			'5': 'Taylor_Swift_1989?id=Bm6l5gvjd6hponuuvzdzk2jwqsm&hl=es'
+		},
+		it: {
+			'1': 'J_AX_Il_bello_d_esser_brutti?id=Blkszxfa6ugfqxqqxyhxc37rpdq',
+			'2': 'Ed_Sheeran_x?id=Bsyd3qgzwutmzucaiknalamq7my',
+			'3': 'Marco_Mengoni_Parole_in_circolo?id=Bokwufzulpbfv3iy24oojz34mce',
+			'4': 'David_Guetta_Listen_Deluxe?id=Byp6lvzimyf74wxi5634ul4tgam',
+			'5': 'Fedez_Pop_hoolista?id=Bgatalrhsjfc6fxhvhabjxx3sue'
 		}
 	},
 	movies: {
@@ -1432,6 +1446,20 @@ var content = {
 			'3': 'Как_приручить_дракона_2?id=8mU89w6c-V0',
 			'4': 'Люси?id=jULgDa6sM9E',
 			'5': 'Стражи_галактики?id=gTp08yAyUBs'
+		},
+		es: {
+			'1': 'Frozen_El_Reino_del_Hielo?id=zqZFa2FfyEg&hl=es',
+			'2': 'La_Lego_Película?id=aphoQqHkAoM&hl=es',
+			'3': 'Aviones_Equipo_de_rescate?id=YlJgFmF4H1w&hl=es',
+			'4': 'El_Hobbit_La_Desolación_de_Smaug?id=yPU6Do8ql3Y&hl=es',
+			'5': 'The_Equalizer_El_Protector_Película_Completa_En_Es?id=1vN4FUwfX0E&hl=es'
+		},
+		it: {
+			'1': 'Frozen?id=HXh2fkhoqJA',
+			'2': 'The_Lego_Movie?id=V9lhm7pb8JI',
+			'3': 'Planes_Fire_Rescue?id=IvDW2AA2wHw',
+			'4': 'The_Grand_Budapest_Hotel?id=u0N7JuD3tYA',
+			'5': 'The_Equalizer_Il_vendicatore?id=10fe5iL96R0'
 		}
 	},
 	apps: {
@@ -1463,6 +1491,20 @@ var content = {
 			'3': 'com.mojang.minecraftpe',
 			'4': 'air.net.machinarium.Machinarium.GP',
 			'5': 'com.deliveryclub'
+		},
+		es: {
+			'1': 'com.aspyr.swkotor&hl=es',
+			'2': 'com.etermax.preguntados.lite&hl=es',
+			'3': 'com.mojang.minecraftpe&hl=es',
+			'4': 'com.wooga.agentalice&hl=es',
+			'5': 'com.king.candycrushsodasaga&hl=es'
+		},
+		it: {
+			'1': 'com.runtastic.android.pro2',
+			'2': 'com.ea.game.fifa15_row',
+			'3': 'com.mojang.minecraftpe',
+			'4': 'com.wooga.agentalice',
+			'5': 'com.king.candycrushsodasaga'
 		}
 	},
 	books: {
@@ -1494,6 +1536,20 @@ var content = {
 			'3': 'Братья_Стругацкие_Трудно_быть_богом?id=XDivinQjn4gC',
 			'4': 'Семенова_М_Волкодав_Мир_по_дороге?id=_MXpAgAAQBAJ',
 			'5': 'Рэй_Брэдбери_451_градус_по_Фаренгейту?id=Vk5XAQAAQBAJ'
+		},
+		es: {
+			'1': 'John_Green_Bajo_la_misma_estrella?id=cvnINtLUcLMC&hl=es',
+			'2': 'Dan_Brown_Inferno_versión_española?id=fYPyHRZeX1UC&hl=es',
+			'3': 'J_R_R_Tolkien_El_Hobbit?id=xi2URRig7jYC&hl=es',
+			'4': 'Ken_Follett_El_umbral_de_la_eternidad_The_Century_?id=3tEQBAAAQBAJ&hl=es',
+			'5': 'Javier_Marías_Así_empieza_lo_malo?id=qf_4AwAAQBAJ&hl=es'
+		},
+		it: {
+			'1': 'John_Green_Colpa_delle_stelle?id=wm1BwsNvs1MC',
+			'2': 'Chiara_Gamberale_Per_dieci_minuti?id=e08bAgAAQBAJ',
+			'3': 'Umberto_Eco_Numero_zero?id=ef7eBQAAQBAJ',
+			'4': 'Robert_Galbraith_Il_baco_da_seta?id=TPmFAwAAQBAJ',
+			'5': 'Veronica_Roth_Divergent?id=UN2XfMUYyiwC'
 		}
 	}
 };
